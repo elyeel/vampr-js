4e07cbac957c5b85b34e19b5d8099312a4dc329d
Risyardi Iswanto <elyeel@yahoo.com>
2021-01-13 17:07:36 -0800

    test 3 vampire passed

--- test/2_numberOfOffspring.js
+++ test/2_numberOfOffspring.js
@@ -1,17 +1,15 @@
-const chai = require('chai');
+const chai = require("chai");
 const expect = chai.expect;
 
-const Vampire = require('../vampire.js');
+const Vampire = require("../vampire.js");
 
-describe("Vampire", function() {
-
+describe("Vampire", function () {
   let rootVampire;
-  beforeEach( function() {
+  beforeEach(function () {
     rootVampire = new Vampire("root");
   });
 
   describe("numberOfOffspring", () => {
-
     it("should get the correct number of offspring", () => {
       expect(rootVampire.numberOfOffspring).to.equal(0);
       rootVampire.addOffspring(new Vampire());
@@ -21,6 +19,7 @@ describe("Vampire", function() {
       rootVampire.addOffspring(new Vampire());
       rootVampire.addOffspring(new Vampire());
       expect(rootVampire.numberOfOffspring).to.equal(5);
+      // console.log(rootVampire);
     });
   });
 });
--- vampire.js
+++ vampire.js
@@ -15,10 +15,20 @@ class Vampire {
   }
 
   // Returns the total number of vampires created by that vampire
-  get numberOfOffspring() {}
+  get numberOfOffspring() {
+    return this.offspring.length;
+  }
 
   // Returns the number of vampires away from the original vampire this vampire is
-  get numberOfVampiresFromOriginal() {}
+  get numberOfVampiresFromOriginal() {
+    let numToRoot = 0;
+    let currVampire = this;
+    while (currVampire.creator) {
+      numToRoot++;
+      currVampire = currVampire.creator;
+    }
+    return numToRoot;
+  }
 
   // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
   isMoreSeniorThan(vampire) {}
