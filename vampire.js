c8c4d67e989361f54ab679115b940a44e7dac7a3
Risyardi Iswanto <elyeel@yahoo.com>
2021-01-14 15:23:15 -0800

    all vampire test passed

--- vampire.js
+++ vampire.js
@@ -38,13 +38,47 @@ class Vampire {
   }
 
   /** Stretch **/
+  parentsList(vampire) {
+    const list = [];
+    // list.push(vampire);
+    const numToRoot = vampire.numberOfVampiresFromOriginal;
+    let curr = vampire;
+    for (let i = 0; i <= numToRoot; ++i) {
+      list.push(curr);
+      curr = curr.creator;
+    }
+    return list;
+  }
 
   // Returns the closest common ancestor of two vampires.
   // The closest common anscestor should be the more senior vampire if a direct ancestor is used.
   // For example:
   // * when comparing Ansel and Sarah, Ansel is the closest common anscestor.
   // * when comparing Ansel and Andrew, Ansel is the closest common anscestor.
-  closestCommonAncestor(vampire) {}
+  closestCommonAncestor(vampire) {
+    if (vampire.name === this.name) return vampire
+
+    const parentOfThis = this.parentsList(this);
+    const parentOfCurr = vampire.parentsList(vampire);
+    let result;
+    if (parentOfThis.length > parentOfCurr.length) {
+      for (let i = 0; i < parentOfThis.length; ++i) {
+        if (parentOfCurr.indexOf(parentOfThis[i]) >= 0) {
+          result = parentOfThis[i];
+          break;
+        }
+      }
+    } else {
+      for (let i = 0; i < parentOfCurr.length; ++i) {
+        if (parentOfThis.indexOf(parentOfCurr[i]) >= 0) {
+          result = parentOfCurr[i];
+          break;
+        }
+      }
+    }
+
+    return result;
+  }
 }
 
 module.exports = Vampire;
